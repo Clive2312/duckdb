6bdd6ca4a32b452e0c57107f1baa9d512c8bbc9a
meliodas137 <mayankgt1043@gmail.com>
2023-06-19 20:14:33 -0400

    custom c++ example added

--- examples/embedded-c++/main.cpp
+++ examples/embedded-c++/main.cpp
@@ -1,5 +1,5 @@
 #include "duckdb.hpp"
-
+#include "iostream"
 using namespace duckdb;
 
 int main() {
@@ -7,8 +7,19 @@ int main() {
 
 	Connection con(db);
 
-	con.Query("CREATE TABLE integers(i INTEGER)");
-	con.Query("INSERT INTO integers VALUES (3)");
-	auto result = con.Query("SELECT * FROM integers");
+	// con.Query("CREATE TABLE customer(Cust_key INTEGER, Name VARCHAR, Account INTEGER, Region VARCHAR)");
+	// con.Query("INSERT INTO customer VALUES (1, 'abc', 221, 'a'), (2, 'bcd', 312,'b'), (3, 'def', 234, 'a'), (4, 'ab', 4234, 'a'), (5, 'bc', 234, 'b'), (6, 'de', 23423, 'b')");
+	// con.Query("EXPORT DATABASE './db' (FORMAT PARQUET);");
+	// con.Query("EXPORT DATABASE './db/csv'");
+	// auto res = con.Query("SELECT * FROM duckdb_extensions()");
+	// res->Print();
+
+	// con.Query("IMPORT DATABASE './db';"); //TODO: Check inmemory database 
+
+	auto result = con.Query("SELECT * FROM parquet_scan('./db/customer.parquet')");
+	// auto result = con.Query("SELECT * FROM parquet_scan('./db/customer.parquet') WHERE Region='a'");
 	result->Print();
+	std::cout<<"Print Enter to continue";
+	while(getchar()!= '\n'){};
+	return 0;
 }
