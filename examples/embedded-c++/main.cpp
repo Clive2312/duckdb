05f38d74409cba50cc72aa869e349afd0e7a47f1
meliodas137 <mayankgt1043@gmail.com>
2023-08-03 19:03:11 -0400

    updated main cpp

--- examples/embedded-c++/main.cpp
+++ examples/embedded-c++/main.cpp
@@ -7,19 +7,32 @@ int main() {
 
 	Connection con(db);
 
-	// con.Query("CREATE TABLE customer(Cust_key INTEGER, Name VARCHAR, Account INTEGER, Region VARCHAR)");
-	// con.Query("INSERT INTO customer VALUES (1, 'abc', 221, 'a'), (2, 'bcd', 312,'b'), (3, 'def', 234, 'a'), (4, 'ab', 4234, 'a'), (5, 'bc', 234, 'b'), (6, 'de', 23423, 'b')");
+	// Create Database with entries
+	// con.Query("CREATE TABLE customer(Cust_key INTEGER, Name VARCHAR, Account INTEGER, Region VARCHAR");
+	// con.Query("INSERT INTO customer VALUES (1, 'abc', 221, 'a'), (2, 'bcd', 312,'a'), (3, 'def', 234, 'b'), (4, 'ab', 4234, 'a'), (5, 'bc', 234, 'b'), (6, 'de', 23423, 'b')");
 	// con.Query("EXPORT DATABASE './db' (FORMAT PARQUET);");
 	// con.Query("EXPORT DATABASE './db/csv'");
+
 	// auto res = con.Query("SELECT * FROM duckdb_extensions()");
 	// res->Print();
 
-	// con.Query("IMPORT DATABASE './db';"); //TODO: Check inmemory database 
+	// auto resu = con.Query("INSTALL 'json';");
+	// db.LoadExtension<>();
+	// auto res = con.Query("SELECT * FROM duckdb_extensions()");
+	// res->Print();
 
-	auto result = con.Query("SELECT * FROM parquet_scan('./db/customer.parquet')");
-	// auto result = con.Query("SELECT * FROM parquet_scan('./db/customer.parquet') WHERE Region='a'");
+	// con.Query("IMPORT DATABASE './db'");
+	// con.Query("INSERT INTO customer VALUES (10, 'abc', NULL, 'a')");
+	
+	// auto result = con.Query("SELECT * FROM customer");
+
+	auto result = con.Query("SELECT * FROM parquet_scan('./db/customer.parquet', policy_file='./policy/rule.json') WHERE Region='a' ");
+	// auto result = con.Query("SELECT * FROM parquet_scan('./db/customer.parquet', policy_file='./policy/rule.json')");
+
 	result->Print();
-	std::cout<<"Print Enter to continue";
-	while(getchar()!= '\n'){};
+
+	// std::cout<<"Print Enter to continue";
+	// while(getchar()!= '\n'){};
+	
 	return 0;
 }
