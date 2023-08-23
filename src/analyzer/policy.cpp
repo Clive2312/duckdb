d94c1668b31d40d6cce408cd4dd04cdcd6fde596
meliodas137 <mayankgt1043@gmail.com>
2023-08-23 12:02:10 -0400

    policy and cond constructors

--- src/analyzer/policy.cpp
+++ src/analyzer/policy.cpp
@@ -1,9 +1,17 @@
 #include "duckdb/analyzer/policy.hpp"
+#include "duckdb/common/enums/expression_type.hpp"
 
 #include "duckdb/common/json/json.h"
 
 namespace duckdb {
-    Policy::Policy(Json::Value &jsonPolicy):val(jsonPolicy){
-        
+    Condition::Condition(Json::Value &condition){
+        auto op = condition["operator"].asString();
+        if(op.compare("aggregate")) type = ExpressionClass::AGGREGATE;
+        for(auto &attr:condition["attributes"]);
+    }
+    Policy::Policy(Json::Value &jsonPolicy):policy(jsonPolicy["policy"]){
+        for(auto &cond: jsonPolicy["conditions"]){
+            conditions.emplace_back(Condition(cond));
+        }
     }
 }
