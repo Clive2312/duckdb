23fa014c258359652dc7edd9aaf30fafeed7418a
meliodas137 <mayankgt1043@gmail.com>
2023-08-23 12:20:21 -0400

    using logical operator type instead of expression type for condition

--- src/analyzer/policy.cpp
+++ src/analyzer/policy.cpp
@@ -1,12 +1,13 @@
 #include "duckdb/analyzer/policy.hpp"
 #include "duckdb/common/enums/expression_type.hpp"
+#include "duckdb/common/enums/logical_operator_type.hpp"
 
 #include "duckdb/common/json/json.h"
 
 namespace duckdb {
     Condition::Condition(Json::Value &condition){
         auto op = condition["operator"].asString();
-        if(op.compare("aggregate")) type = ExpressionClass::AGGREGATE;
+        if(op.compare("aggregate")) this->op = LogicalOperatorType::LOGICAL_AGGREGATE_AND_GROUP_BY;
         for(auto &attr:condition["attributes"]);
     }
     Policy::Policy(Json::Value &jsonPolicy):policy(jsonPolicy["policy"]){
