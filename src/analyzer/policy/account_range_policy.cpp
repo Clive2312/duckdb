9f3e91f35a37e047d1444b326859c39d965162b8
meliodas137 <mayankgt1043@gmail.com>
2023-12-12 20:43:59 -0500

    using states for policy checking

--- src/analyzer/policy/account_range_policy.cpp
+++ src/analyzer/policy/account_range_policy.cpp
@@ -1,4 +1,5 @@
 #include "duckdb/analyzer/policy/account_range_policy.hpp"
+#include "duckdb/analyzer/states/count_row_state.hpp"
 #include <iostream>
 using namespace std::placeholders;
 namespace duckdb {
@@ -8,6 +9,7 @@ AccountRangePolicy::AccountRangePolicy(): PolicyFunction(){
 unique_ptr<LogicalOperator> AccountRangePolicy::modifyPlan(unique_ptr<LogicalOperator> op) {
     if(op->type == LogicalOperatorType::LOGICAL_AGGREGATE_AND_GROUP_BY) {
         op->inputCheckers.emplace_back(std::bind(&AccountRangePolicy::inputChecker, *this, _1));
+        op->states.emplace_back(make_uniq<CountRowState>(1, Value(0)));
     }
 
     for(auto &child: op->children) {
@@ -17,17 +19,20 @@ unique_ptr<LogicalOperator> AccountRangePolicy::modifyPlan(unique_ptr<LogicalOpe
     return op;
 }
 
-bool AccountRangePolicy::inputChecker(DataChunk &input) {
+bool AccountRangePolicy::inputChecker(DataChunk &data) {
     
-    for(int i = 0; i < input.size(); i++) {
-        auto val = input.GetValue(0, i);
-        if(val <= Value(300)) {
-            return false;
-        }
+    // for(int i = 0; i < input.size(); i++) {
+    //     auto val = input.GetValue(0, i);
+    //     if(val <= Value(300)) {
+    //         return false;
+    //     }
+    // }
+    if(data.store->GetStateValue(1) > 1) {
+        return false;
     }
-
+    
     return true;
-}
+}                           
 
 bool AccountRangePolicy::matchCondition(LogicalOperator &op) {
     if(op.type == LogicalOperatorType::LOGICAL_AGGREGATE_AND_GROUP_BY) {
