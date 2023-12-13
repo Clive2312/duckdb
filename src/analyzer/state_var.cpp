90ebb2cdd557e4a4f5dbcb40d4f73ce649527767
meliodas137 <mayankgt1043@gmail.com>
2023-12-12 20:29:35 -0500

    added statevar base class

---
+++ src/analyzer/state_var.cpp
@@ -0,0 +1,17 @@
+#include "duckdb/analyzer/state_var.hpp"
+
+namespace duckdb {
+
+StateVar::StateVar(int id): id(id){}
+
+StateVar::StateVar(int id, Value val): id(id), val(val) {}
+
+void StateVar::Collector(DataChunk &data) {
+    return;
+}
+
+void StateVar::Combiner(vector<Value> &local_values) {
+    return;
+}
+
+};
---
+++ src/include/duckdb/analyzer/state_var.hpp
@@ -0,0 +1,28 @@
+//===----------------------------------------------------------------------===//
+//                         DuckDB
+//
+// duckdb/analyzer/state_var.hpp
+//
+//
+//===----------------------------------------------------------------------===//
+#pragma once
+
+#include "duckdb/planner/logical_operator.hpp"
+#include "duckdb/common/types/data_chunk.hpp"
+
+namespace duckdb{
+
+class StateVar {
+
+public:
+    int id;
+    Value val;
+
+    StateVar(int id);
+    StateVar(int id, Value val);
+
+    virtual void Collector(DataChunk &data);
+    virtual void Combiner(vector<Value> &local_values);
+};
+
+};
