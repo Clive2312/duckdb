d7661c3535f66c66a1dd49261dbaccb5ee1eb1c0
meliodas137 <mayankgt1043@gmail.com>
2023-12-12 20:31:10 -0500

    added state store file to be used in datachunk

---
+++ src/common/types/state_store.cpp
@@ -0,0 +1,44 @@
+#include "duckdb/common/types/state_store.hpp"
+
+namespace duckdb{
+
+StateStore::StateStore() {};
+
+Value StateStore::GetStateValue(int id) {
+    if(store.find(id) == store.end()) {
+        return Value(0);
+    }
+    return store[id].back(); //fix this for using combiner function
+};
+
+void StateStore::SetStateValue(int id, Value &val) {
+    if(store.find(id) == store.end()) {
+        store[id] = {};
+    }
+
+    store[id].push_back(val);
+    return;
+}
+
+
+void StateStore::MergeStore(StateStore &other) {
+    for(auto &entry: other.store){
+        if(store.find(entry.first) == store.end()) {
+            store[entry.first] = entry.second;
+        }
+        else {
+            for(auto &val: entry.second) {
+                store[entry.first].emplace_back(val);
+            }
+        }
+    } 
+
+    return;
+}
+
+void StateStore::Move(unique_ptr<StateStore> other){
+    this->MergeStore(*other);
+    return;
+}
+
+}
---
+++ src/include/duckdb/common/types/state_store.hpp
@@ -0,0 +1,27 @@
+//===----------------------------------------------------------------------===//
+//                         DuckDB
+//
+// duckdb/common/types/state_store.hpp
+//
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once 
+
+#include "duckdb/common/unordered_map.hpp"
+#include "duckdb/common/types/value.hpp"
+
+namespace duckdb {
+
+class StateStore {
+    public:
+    StateStore();
+    Value GetStateValue(int id);
+    void SetStateValue(int id, Value &val);
+    void MergeStore(StateStore &other);
+    void Move(unique_ptr<StateStore> other);
+    public:
+    unordered_map<int, vector<Value>> store;
+
+};
+}
