fffc79e430aed18b74e365e9996db5e2d60e6216
meliodas137 <mayankgt1043@gmail.com>
2024-03-06 16:31:02 -0500

    removing dead code

--- src/analyzer/analyzer.cpp
+++ src/analyzer/analyzer.cpp
@@ -1,9 +1,6 @@
 #include "duckdb/analyzer/policy.hpp"
 #include "duckdb/analyzer/analyzer.hpp"
 #include "fstream"
-// #include "duckdb/analyzer/policy/account_range_policy.hpp"
-// #include "duckdb/planner/operator/logical_comparison_join.hpp"
-// using namespace std::placeholders;
 
 #define MATCH "match"
 #define POLICY_SQL "policy_sql"
@@ -47,7 +44,6 @@ void Analyzer::generatePolicyInstances(Json::Value policy) {
         if(isPlaceholder(key))
         {
             keys.emplace_back(key);
-            std::cout<<key<<'\n';
             placeholder_map[key] = queryDOM.select_nodes(it->asCString());
         }
     }
@@ -89,117 +85,5 @@ vector<string> Analyzer::createAllCombinations(vector<string> &keys, unordered_m
 
     return completed_policies;
 }
-
-// Analyzer::Analyzer(){
-//     policyFunctions.emplace_back(make_shared<AccountRangePolicy>());
-// }
-
-// unique_ptr<LogicalOperator> Analyzer::modifiedPlan(unique_ptr<LogicalOperator> op) {
-//     for(auto &policy: policyFunctions) {
-//         op = policy->getModifiedPlan(std::move(op));
-//     }
-//     return op;
-// }
-
-// Analyzer::Analyzer(Json::Value &policies_json){
-//     for(auto &policy: policies_json) {
-//         policies.emplace_back(make_shared<Policy>(policy));
-//     }
-// }
-
-// string GetName(Value &attrib) {
-//     string val = attrib.ToString();
-//     if(!val.empty()) {
-//         if(val[0] != '@') {
-//             return StringUtil::Lower(val);
-//         }
-//         else {
-//             return StringUtil::Lower(val.substr(1, val.size() - 1));
-//         }
-//     }
-//     return "";
-// }
-
-// bool CompareTables(ConditionAST* cond, LogicalOperator &op){
-// 	if(op.type == LogicalOperatorType::LOGICAL_GET) {
-// 		return StringUtil::Lower(op.ParamsToString()) == GetName(cond->attribute);
-// 	}
-// 	return false;
-// }
-
-// bool TreeMatcher(ConditionAST* cond, LogicalOperator &op){
-//     if(cond == nullptr) return true;
-    
-//     if(op.type == cond->logical_op || CompareTables(cond, op)) {
-
-//         bool leftMatch = false, rightMatch = false;
-
-//         for(int i = 0; i < op.children.size(); i++) {
-//             if(!leftMatch && TreeMatcher(cond->l_child, *op.children[i])){
-//                 leftMatch = true;
-//             }
-
-//             if(!rightMatch && TreeMatcher(cond->r_child, *op.children[i])){
-//                 rightMatch = true;
-//             }
-
-//             if(leftMatch && rightMatch) {
-//                 return true;
-//             }
-//         }
-//         if(op.children.empty()) return true;
-//     } 
-//     if(op.children.empty()) return false;
-
-//     bool matched = false;
-//     for(int i = 0; i < op.children.size(); i++) {
-//         if(!matched && TreeMatcher(cond, *op.children[i])){
-//             matched = true;
-//         }
-//         if(matched) {
-//             return true;
-//         }
-//     }
-//     return false;
-// }
-
-// bool MatchConditions(vector<shared_ptr<Condition>> &conditions, LogicalOperator &plan){
-//     for(auto &cond: conditions) {
-//         if(cond->mode == CondPolicyMode::SQL_MATCH && TreeMatcher(cond->expr, plan)) return true;
-//     }
-//     return false;
-// }
-
-// vector<shared_ptr<Policy>> Analyzer::ConditionMatcher(LogicalOperator &plan) {
-//     vector<shared_ptr<Policy>> res = {};
-//     for(auto &policy: policies){
-//         if(MatchConditions(policy->conditions, plan)){
-//             res.emplace_back(policy);
-//         }
-//     }
-//     return res;
-// }
-
-// void InsertIntoOps(shared_ptr<Action> action, LogicalOperator &op){
-//     if(op.type == action->op) {
-//         op.actions.emplace_back(action);
-//     } 
-
-//     for(auto &child: op.children) {
-//         InsertIntoOps(action, *child);
-//     }
-// }
-
-// unique_ptr<LogicalOperator> Analyzer::MatchAndInsertPolicies(unique_ptr<LogicalOperator> plan) {
-//     auto policies = ConditionMatcher(*plan);
-
-//     for(auto policy: policies) {
-//         for(auto &action: policy->data_actions) {
-//             InsertIntoOps(action, *plan);
-//         }
-//     }
-//     return std::move(plan);
-// }
-
 
 }
--- src/include/duckdb/analyzer/analyzer.hpp
+++ src/include/duckdb/analyzer/analyzer.hpp
@@ -17,16 +17,13 @@
 #include "duckdb/analyzer/policy/account_range_policy.hpp"
 #include "duckdb/common/pugixml.hpp"
 #include "duckdb/parser/sql_statement.hpp"
-// #include "duckdb/analyzer/policy_function.hpp"
 
-#include <iostream>
 #include <functional>
 
 namespace duckdb {
 
 class Analyzer {
 public:
-	// vector<shared_ptr<Policy>> policies;
 	vector<shared_ptr<PolicyFunction>> policyFunctions;
 	string policies;
 
@@ -36,20 +33,11 @@ public:
 
 	string getPolicies();
 
-	// unique_ptr<LogicalOperator> modifiedPlan(unique_ptr<LogicalOperator> op);
-	// void VisitOperator(LogicalOperator &op);
-	// unique_ptr<LogicalOperator> MatchAndInsertPolicies(unique_ptr<LogicalOperator> plan);
-
 private:
 	string policy_file = "./policy.json";
 	pugi::xml_document queryDOM;
     vector<string> createAllCombinations(vector<string> &keys, unordered_map<string, pugi::xpath_node_set> &placeholder_map, Json::Value &policy_sql);
 	void generatePolicyInstances(Json::Value policy);
-	// vector<Json::Value> policies;
-	// void ModifyPlan(LogicalOperator &plan, Policy policy);
-	// bool everything_referenced = false;
-	// vector<shared_ptr<Policy>> ConditionMatcher(LogicalOperator &plan);
-	// unordered_map<uint8_t, int> operators;
 };
 
 } // namespace duckdb
