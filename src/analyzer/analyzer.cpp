04fd8b9637db05f2a63c451c756b2c8480908de5
meliodas137 <mayankgt1043@gmail.com>
2024-03-06 16:24:36 -0500

    analyzer class running matches and generating possible policy violation using xml parser

--- src/analyzer/analyzer.cpp
+++ src/analyzer/analyzer.cpp
@@ -1,20 +1,105 @@
 #include "duckdb/analyzer/policy.hpp"
 #include "duckdb/analyzer/analyzer.hpp"
+#include "fstream"
 // #include "duckdb/analyzer/policy/account_range_policy.hpp"
 // #include "duckdb/planner/operator/logical_comparison_join.hpp"
-using namespace std::placeholders;
+// using namespace std::placeholders;
+
+#define MATCH "match"
+#define POLICY_SQL "policy_sql"
+#define PLACEHOLDER '$'
 
 namespace duckdb {
-Analyzer::Analyzer(){
-    policyFunctions.emplace_back(make_shared<AccountRangePolicy>());
+
+Analyzer::Analyzer(SQLStatement& query){
+    queryDOM.load_string((query.ToXMLString()).c_str());
 }
 
-unique_ptr<LogicalOperator> Analyzer::modifiedPlan(unique_ptr<LogicalOperator> op) {
-    for(auto &policy: policyFunctions) {
-        op = policy->getModifiedPlan(std::move(op));
+string Analyzer::getPolicies(){
+    if(policies.size() == 0) {
+        std::ifstream handle(policy_file);
+		Json::Reader reader;
+		Json::Value completeJson;
+		reader.parse(handle, completeJson);
+
+        for(auto &policy: completeJson){
+		    auto matched = queryDOM.select_node(policy[MATCH].asCString()).node();
+            if(matched) {
+                generatePolicyInstances(policy);
+            }
+	    }
     }
-    return op;
+    return policies;
 }
+
+bool isPlaceholder(string key){
+    if(key.size() > 0 && key[0] == PLACEHOLDER) return true;
+    return false;
+}
+
+void Analyzer::generatePolicyInstances(Json::Value policy) {
+    auto it = policy.begin();
+    unordered_map<string, pugi::xpath_node_set> placeholder_map;
+    vector<string> keys;
+
+    for( ;it != policy.end(); it++) {
+        string key = it.key().asString();
+        if(isPlaceholder(key))
+        {
+            keys.emplace_back(key);
+            std::cout<<key<<'\n';
+            placeholder_map[key] = queryDOM.select_nodes(it->asCString());
+        }
+    }
+
+    vector<string> completed_policies = createAllCombinations(keys, placeholder_map, policy[POLICY_SQL]);
+
+    for(auto &p: completed_policies) {
+        policies += p;
+    }
+}
+
+vector<string> Analyzer::createAllCombinations(vector<string> &keys, unordered_map<string, pugi::xpath_node_set> &placeholder_map, Json::Value &policy_sql){
+    if(keys.empty()) return { policy_sql.asCString() };
+
+    auto lastkey = keys.back();
+    keys.pop_back();
+
+    auto partial_policies = createAllCombinations(keys, placeholder_map, policy_sql);
+    vector<string> completed_policies = {};
+
+    for(auto policy: partial_policies) {
+        auto lastkey_pos = policy.find(lastkey);
+    
+        if(lastkey_pos != string::npos) {
+            auto it = placeholder_map[lastkey].begin();
+
+            for(; it != placeholder_map[lastkey].end(); it++) {
+
+                std::stringstream ss;
+                it->node().nodeToSQL(ss);
+
+                auto completed_policy = policy.replace(lastkey_pos, lastkey.size(), ss.str());
+                completed_policies.emplace_back(completed_policy);
+            }
+        } else {
+            completed_policies.emplace_back(policy);
+        }
+    }
+
+    return completed_policies;
+}
+
+// Analyzer::Analyzer(){
+//     policyFunctions.emplace_back(make_shared<AccountRangePolicy>());
+// }
+
+// unique_ptr<LogicalOperator> Analyzer::modifiedPlan(unique_ptr<LogicalOperator> op) {
+//     for(auto &policy: policyFunctions) {
+//         op = policy->getModifiedPlan(std::move(op));
+//     }
+//     return op;
+// }
 
 // Analyzer::Analyzer(Json::Value &policies_json){
 //     for(auto &policy: policies_json) {
--- src/include/duckdb/analyzer/analyzer.hpp
+++ src/include/duckdb/analyzer/analyzer.hpp
@@ -15,6 +15,8 @@
 #include "duckdb/common/types/vector.hpp"
 #include "duckdb/common/unordered_map.hpp"
 #include "duckdb/analyzer/policy/account_range_policy.hpp"
+#include "duckdb/common/pugixml.hpp"
+#include "duckdb/parser/sql_statement.hpp"
 // #include "duckdb/analyzer/policy_function.hpp"
 
 #include <iostream>
@@ -24,21 +26,30 @@ namespace duckdb {
 
 class Analyzer {
 public:
-	vector<shared_ptr<Policy>> policies;
+	// vector<shared_ptr<Policy>> policies;
 	vector<shared_ptr<PolicyFunction>> policyFunctions;
+	string policies;
 
 public:
 	Analyzer();
-	// Analyzer(Json::Value &policies_json);
-	unique_ptr<LogicalOperator> modifiedPlan(unique_ptr<LogicalOperator> op);
-	void VisitOperator(LogicalOperator &op);
-	unique_ptr<LogicalOperator> MatchAndInsertPolicies(unique_ptr<LogicalOperator> plan);
+	Analyzer(SQLStatement &query);
+
+	string getPolicies();
+
+	// unique_ptr<LogicalOperator> modifiedPlan(unique_ptr<LogicalOperator> op);
+	// void VisitOperator(LogicalOperator &op);
+	// unique_ptr<LogicalOperator> MatchAndInsertPolicies(unique_ptr<LogicalOperator> plan);
 
 private:
-	void ModifyPlan(LogicalOperator &plan, Policy policy);
-	bool everything_referenced = false;
-	vector<shared_ptr<Policy>> ConditionMatcher(LogicalOperator &plan);
-	unordered_map<uint8_t, int> operators;
+	string policy_file = "./policy.json";
+	pugi::xml_document queryDOM;
+    vector<string> createAllCombinations(vector<string> &keys, unordered_map<string, pugi::xpath_node_set> &placeholder_map, Json::Value &policy_sql);
+	void generatePolicyInstances(Json::Value policy);
+	// vector<Json::Value> policies;
+	// void ModifyPlan(LogicalOperator &plan, Policy policy);
+	// bool everything_referenced = false;
+	// vector<shared_ptr<Policy>> ConditionMatcher(LogicalOperator &plan);
+	// unordered_map<uint8_t, int> operators;
 };
 
 } // namespace duckdb
