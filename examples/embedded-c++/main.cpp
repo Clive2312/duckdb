a3f4f318e51cc754d8945805390629670ec76c76
meliodas137 <mayankgt1043@gmail.com>
2024-03-11 14:57:24 -0400

    added new policy and sql test case

--- examples/embedded-c++/main.cpp
+++ examples/embedded-c++/main.cpp
@@ -6,33 +6,22 @@ int main() {
 	DuckDB db(nullptr);
 
 	Connection con(db);
-
-	// Create Database with entries
-	// con.Query("CREATE TABLE customer(Cust_key INTEGER, Name VARCHAR, Account INTEGER, Region VARCHAR");
-	// con.Query("INSERT INTO customer VALUES (1, 'abc', 221, 'a'), (2, 'bcd', 312,'a'), (3, 'def', 234, 'b'), (4, 'ab', 4234, 'a'), (5, 'bc', 234, 'b'), (6, 'de', 23423, 'b')");
-	// con.Query("EXPORT DATABASE './db' (FORMAT PARQUET);");
-	// con.Query("EXPORT DATABASE './db/csv'");
-
-	// auto res = con.Query("SELECT * FROM duckdb_extensions()");
-	// res->Print();
-
-	// auto resu = con.Query("INSTALL 'json';");
-	// db.LoadExtension<>();
-	// auto res = con.Query("SELECT * FROM duckdb_extensions()");
-	// res->Print();
-
-	// con.Query("IMPORT DATABASE './db'");
-	// con.Query("INSERT INTO customer VALUES (10, 'abc', NULL, 'a')");
 	
-	// auto result = con.Query("SELECT * FROM customer");
-	// auto result = con.Query("COPY (SELECT * FROM read_csv_auto('./ratings.csv')) TO 'ratings.parquet' (FORMAT PARQUET);");
-	// auto result = con.Query("SELECT * FROM parquet_scan('./db/customer.parquet', policy_file='./policy/rule.json') WHERE Region='a' ");
-	// auto result = con.Query("SELECT SUM(Account) FROM parquet_scan('./db/customer.parquet')");
+	// Test Query 1 -- Only allow aggregates when row count greater than base limit
 
-	// auto result = con.Query("SELECT AVG(time), AVG(quantity), AVG(price), SUM(price), Count(price) FROM parquet_scan('./trade.parquet') WHERE price > 300 AND quantity < 4000");
+	// auto result = con.Query("SELECT Sum(rating) From './ratings.parquet' WHERE rating > 4.8");
 
-	auto result = con.Query("SELECT Count(*), AVG(time), AVG(quantity), MEDIAN(quantity), AVG(price), SUM(price), Avg(rating) FROM parquet_scan('./ratings.parquet') as R JOIN parquet_scan('./trade.parquet') as T ON R.stocksymbol = T.stocksymbol WHERE price > 300 AND quantity < 4000");
+	// Test Query 2 -- Group by only allowed when each group has row count greater than base limit
 
-	result->Print();
+	// auto result = con.Query("Select Avg(price), Count(*) From './trade.parquet' WHERE quantity > 3000 GROUP BY stocksymbol;");
+	// auto result = con.Query("Select Avg(price), Count(*) From './trade.parquet' WHERE quantity > 2000 GROUP BY stocksymbol;");
+	
+	// Test Query 3 -- If there is a join, can't access data past certain time
+
+	auto result = con.Query("Select * FROM './trade.parquet' JOIN './ratings.parquet' ON trade.stocksymbol = ratings.stocksymbol WHERE time > 1000;");
+
+
+	// result->Print();
+	
 	return 0;
 }
